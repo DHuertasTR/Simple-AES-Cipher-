56f19d4fb93c9d947afbb8e871ef7fb461eca891
david huertas <aletru321@gmail.com>
2021-05-18 22:11:25 -0500

    fixed things

---
+++ Main.py
@@ -0,0 +1,26 @@
+def desciph(pathFile, password):
+    print("file recover")
+
+def ciph(pathFile, password):
+    print("response ciphered")
+    
+def main():
+    response= int(input("Ingrese 1 si desea cifrar un archivo, ingrese 2 si desea descifrar un archivo o CTR+c si desea salir"))
+    if response==1:
+        pathFile=input("ingrese la ruta del archivo")
+        password=input("ingrese una contrasena")
+        
+        ciph(pathFile, password)
+    
+    elif response==2:
+        pathFile=input("ingrese la ruta del archivo")
+        password=input("ingrese una contrasena")
+        
+        desciph(pathFile, password)
+    
+    else:
+        print("ingrese un numero valido")
+
+if __name__ == "__main__":
+    main()
+    
