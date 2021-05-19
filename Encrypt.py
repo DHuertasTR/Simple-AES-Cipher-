bd8f81efaeaa5d28c4af1577b709bcb9e0271417
david huertas <aletru321@gmail.com>
2021-05-19 00:29:21 -0500

    encrypt func ready

---
+++ Encrypt.py
@@ -0,0 +1,32 @@
+from Crypto.Cipher import AES
+import os
+import hashlib as hl
+
+class Cipher:
+    def __init__(self):
+        pass
+    
+    def padToAES(self, entry):
+       # entry=entry.encode('utf-8')
+        while len(entry) % AES.block_size!=0:
+            entry=entry + b'0'
+        return entry
+    
+    def cipher(self, password, file):
+        salt=os.urandom(8) 
+        key= hl.pbkdf2_hmac('sha256',password.encode('utf-8'), salt, 400)
+        mode= AES.MODE_CBC
+        IV= os.urandom(16)
+        
+        cc= AES.new(key,mode,IV)
+        with open(file,'rb') as r:
+            oldFile=r.read()
+        
+        oldFile=self.padToAES(oldFile)
+        encryptedFile=cc.encrypt(oldFile)
+        
+        with open(file+'.enc','wb') as w:
+            w.write(encryptedFile)
+        
+    
+    
--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
+from Crypto import Cipher
+from Encrypt import Cipher as cp
+
+
 def desciph(pathFile, password):
     print("file recover")
 
@@ -5,6 +9,11 @@ def ciph(pathFile, password):
     print("response ciphered")
     
 def main():
+    
+    cpp=cp()
+    
+    print(cpp.cipher('come','image.jpg'))
+    
     response= int(input("Ingrese 1 si desea cifrar un archivo, ingrese 2 si desea descifrar un archivo o CTR+c si desea salir"))
     if response==1:
         pathFile=input("ingrese la ruta del archivo")
